20b3fd521ff808543fd1493b8cac29aa9b34dadf
KevinFrutos <vinke.14297@gmail.com>
2022-11-14 21:12:22 +0100

    FEED-6: Request para recuperar los datos del producto por EAN13 o EAN8

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiController extends Controller
+{
+    public const HTTP_BAD_REQUEST = 'BAD REQUEST';
+
+    /**
+     * @param string $ean
+     * @return PromiseInterface|\Illuminate\Http\Client\Response|Response
+     */
+    public function getProductByEAN(string $ean): PromiseInterface|\Illuminate\Http\Client\Response|Response
+    {
+        if ($this->validateEANCode($ean)) {
+            $response = Http::acceptJson()->get("https://world.openfoodfacts.org/api/v0/product/$ean.json");
+            return $this->verifyResponseStatus($response);
+        }
+        return new Response(self::HTTP_BAD_REQUEST, Response::HTTP_BAD_REQUEST, ['content-type' => 'text/html']);
+    }
+
+    /**
+     * @param string $ean
+     * @return bool
+     */
+    private function validateEANCode(string $ean): bool
+    {
+        return (strlen($ean) == 8 || strlen($ean) == 13);
+    }
+
+    /**
+     * @param PromiseInterface|\Illuminate\Http\Client\Response|Response $response
+     * @return PromiseInterface|\Illuminate\Http\Client\Response|Response
+     */
+    private function verifyResponseStatus(PromiseInterface|\Illuminate\Http\Client\Response|Response $response): PromiseInterface|\Illuminate\Http\Client\Response|Response
+    {
+        if (isset($response['status']) && $response['status'] == 0) {
+            $response = new Response(self::HTTP_BAD_REQUEST, Response::HTTP_BAD_REQUEST, ['content-type' => 'text/html']);
+        }
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/v1/product/{ean}', [ApiController::class, 'getProductByEAN'])->name('api.product.get');
 
 Route::middleware([
     'auth:sanctum',
