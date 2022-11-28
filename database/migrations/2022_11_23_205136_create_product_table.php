eb29f228083e8b514de1bbe4c0ae1bc584aa450a
Samuel Araujo <samuel.araujo@studiogenesis.es>
2022-11-28 19:50:36 +0100

    Add Product, Nutriscore Grade, Nova Group tables

---
+++ database/migrations/2022_11_23_205136_create_product_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('quantity');
+            $table->string('EAN');
+            $table->unsignedBigInteger('nutriscore_grade_id');
+            $table->foreign('nutriscore_grade_id')
+                    -> references('id')
+                    -> on('nutriscore_grade');
+            $table->unsignedBigInteger('nova_group_id');
+            $table->foreign('nova_group_id')
+                    -> references('id')
+                    -> on('nova_group');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
