79839afddd0022f42aab047ec93b81f7c6bd8ca7
Oriol25 <oriol.poveda@insbaixcamp.cat>
2023-01-23 18:58:46 +0100

    add logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,13 +5,17 @@
     </ul>
 </div>
 <div id="secondHeader">
-    <img class="no-selected" src="{{ asset('./assets/off-logo-horizontal-light.svg') }}" alt="">
+    <div class="d-flex gap-3">
+        <img class="no-selected align-self-center" width="32px" height="32px" src="{{ asset('./assets/logo.png') }}"
+            alt="">
+        <h1 class="align-self-center">Fe<span class="text-warning font-italic">ED</span>me</h1>
+    </div>
     <div>
         <form method="POST" action="{{ route('home.searchFromApi') }}">
             @csrf
             <input type="search" name="home_search" placeholder="Busca un producto" id="searcher" maxlength="13">
             <button id="search" type="submit"><i class="fas fa-search"></i></button>
-        </form>        
+        </form>
     </div>
     <button id="scanner">SCANNER</button>
 
