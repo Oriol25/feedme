02d9527f3c29bd156718cd3694b13843bfb6cb79
Oriol25 <oriol.poveda@insbaixcamp.cat>
2023-01-23 17:59:34 +0100

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,35 @@
+<footer>
+    <div id="footer1" class="rounded" style="background-color: rgb(255, 229, 199); padding: 40px;">
+        <div class="row align-items-start">
+            <div class="col">
+                <h4> Join the community </h4>
+                <ul style="list-style: none;padding-left: 0px;">
+                    <li>Descobreix el nostre <a href="#" class="text-dark">codi de conducta</a></li>
+                    <li>Uneix-te a <a href="#" class="text-dark"> Slack </a></li>
+                    <li><a href="#" class="text-dark"> Forum </a></li>
+                    <li>Segueix-nos <a href="#" class="text-dark">Twitter</a> <a href="#"
+                            class="text-dark">Facebook</a> <a href="#" class="text-dark">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4> Discover Projects </h4>
+                <button style="margin:5px" class="btn btn-light"> Qui Som Feed Me </button>
+                <button style="margin:5px" class="btn btn-light"> Visió, Missió, Valors i Programes </button>
+                <button style="margin:5px" class="btn btn-light"> Preguntes Freqüents </button>
+            </div>
+        </div>
+    </div>
+    <div id="footer2" style="background-color: slategray;padding: 30px;">
+        <div class="text-center text-white">
+            <h1>Feed Me</h1>
+            <p>Una base de dades, per veure els alergens dels teus aliments</p>
+            <ul class="row nav">
+                <li class="col"><a href="#" class="text-white">Legal</a></li>
+                <li class="col"><a href="#" class="text-white">Terms of use</a></li>
+                <li class="col"><a href="#" class="text-white">Data, API and SDKs</a></li>
+                <li class="col"><a href="#" class="text-white">Donate to FeedMe</a></li>
+                <li class="col"><a href="#" class="text-white">Producers</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
--- resources/views/theme/main.blade.php
+++ resources/views/theme/main.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @include('partials.metas')
-        @include('partials.css')
-        @stack('css')
-    </head>
-    <body>
-        @include('partials.header')
-        
-        <div class="content">
-            @yield('content')
-        </div>
-        <div class="footer">
-            @include('partials.footer')
-        </div>
-        @include('partials.js')
-        @stack('js')        
-    </body>
+
+<head>
+    @include('partials.metas')
+    @include('partials.css')
+    @stack('css')
+</head>
+
+<body>
+    @include('partials.header')
+
+    <div class="content">
+        @yield('content')
+    </div>
+
+    @include('partials.footer')
+
+    @include('partials.js')
+    @stack('js')
+</body>
+
 </html>
