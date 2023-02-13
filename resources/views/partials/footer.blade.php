37591f6895fc5f6b931751ec8e74747e7b38a836
Oriol25 <oriol.poveda@insbaixcamp.cat>
2023-02-13 17:38:34 +0100

    change footer texts

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,17 +2,17 @@
     <div id="footer" class="rounded mt-5">
         <div class="row align-items-start">
             <div class="col-md-6 col-sm-12">
-                <h4> Join the community </h4>
+                <h4> Uneix-te a la comunitat </h4>
                 <ul>
                     <li>Descobreix el nostre <a href="#" class="text-dark">codi de conducta</a></li>
                     <li>Uneix-te a <a href="#" class="text-dark"> Slack </a></li>
                     <li><a href="#" class="text-dark"> Forum </a></li>
-                    <li>Segueix-nos <a href="#" class="text-dark">Twitter</a> <a href="#"
+                    <li>Segueix-nos a<a href="#" class="text-dark">Twitter</a> <a href="#"
                             class="text-dark">Facebook</a> <a href="#" class="text-dark">Instagram</a></li>
                 </ul>
             </div>
             <div class="col-md-6 col-sm-12">
-                <h4> Discover Projects </h4>
+                <h4>El nostre projecte</h4>
                 <button class="btn btn-light"> Qui Som Feed Me </button>
                 <button class="btn btn-light"> Visió, Missió, Valors i Programes </button>
                 <button class="btn btn-light"> Preguntes Freqüents </button>
