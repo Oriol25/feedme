8502e8a77c63e115f21f43a5a5e0129d202c1082
Oriol25 <oriol.poveda@insbaixcamp.cat>
2023-02-13 17:15:19 +0100

    card size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,18 +13,21 @@
                         <div class="card card-size">
                             <a href="{{ route('show.product', ['name' => $product->title, 'code' => $product->EAN, 'product' => $product->id]) }}"
                                 class="text-reset text-decoration-none">
-                                <div class="list_product_img_div">
-                                    <img class="card-img-top img-front list_product_img_div" src="{{ $product->image }}"
-                                        alt="{{ $product->title }}">
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title text-center">{{ $product->title }} - {{ $product->quantity }}</h6>
-                                </div>
-                                <div class="card-footer">
-                                    <img class="w-25 img-responsive" src="{{ $product->nutriscoreGrade->img }}"
-                                        alt="Nutriscore Grade: {{ $product->nutriscoreGrade->name }}">
-                                    <img class="img-responsive" width="16px" src="{{ $product->novaGroup->img }}"
-                                        alt="{{ $product->novaGroup->name }}">
+                                <div class="d-flex flex-column justify-content-between">
+                                    <div class="list_product_img_div">
+                                        <img class="card-img-top img-front list_product_img_div" src="{{ $product->image }}"
+                                            alt="{{ $product->title }}">
+                                    </div>
+                                    <div class="card-body">
+                                        <h6 class="card-title text-center">{{ $product->title }} - {{ $product->quantity }}
+                                        </h6>
+                                    </div>
+                                    <div class="card-footer">
+                                        <img class="w-25 img-responsive" src="{{ $product->nutriscoreGrade->img }}"
+                                            alt="Nutriscore Grade: {{ $product->nutriscoreGrade->name }}">
+                                        <img class="img-responsive" width="16px" src="{{ $product->novaGroup->img }}"
+                                            alt="{{ $product->novaGroup->name }}">
+                                    </div>
                                 </div>
                             </a>
                         </div>
